fe650cf28e645f9984b772f62b4554cb0b9166cf
joshuajhun <joshuajhun@gmail.com>
2018-03-19 11:40:02 -0600

    renamed tests so theyre more descriptive

--- filter/filter-test.js
+++ filter/filter-test.js
@@ -2,7 +2,7 @@ import 'locus'
 import { expect } from 'chai'
 describe('filter tests', () => {
 
-  it.skip('should pick the even numbers ', () => {
+  it.skip('should return a new array of only even numbers ', () => {
     const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
     // your code goes here
@@ -10,7 +10,7 @@ describe('filter tests', () => {
     expect(results).to.deep.equal([2, 4, 6, 8, 10]);
   });
 
-  it.skip('should pick the odd numbers ', () => {
+  it.skip('should return a new array of only odd numbers ', () => {
     const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
    
     // your code goes here
@@ -18,7 +18,7 @@ describe('filter tests', () => {
     expect(results).to.deep.equal([1, 3, 5, 7, 9]);
   });
 
-  it.skip('should pick words with three characters', () => {
+  it.skip('should return a new array of words with three characters', () => {
     const words = ["pill", "bad", "finger", "cat", "blue", "dog", "table", "red"];
 
     // your code goes here
@@ -26,7 +26,7 @@ describe('filter tests', () => {
     expect(results).to.deep.equal(["bad", "cat", "dog", "red"]);
   });
 
-  it.skip('should pick arrays only', () => {
+  it.skip('should return a new array of arrays', () => {
     const elements = ["CAT", ["dog"], 23, [56, 3, 8], "AIMLESS", 43, "butter"];
 
     // your code goes here
@@ -34,32 +34,38 @@ describe('filter tests', () => {
     expect(results).to.deep.equal([["dog"], [56, 3, 8]]);
   });
 
-  it.skip('should remove specific array from object', () => {
-    const human = { name: 'yung-jhun', songs:[['cool song 1', 50], ['cool song 2', 100], ['cool song 3', 90]] };
+  it.skip('should a new object with cool song 2 removed', () => {
+    const human = {
+                    name: 'yung-jhun', songs:[['cool song 1', 50], 
+                                              ['cool song 2', 100], 
+                                              ['cool song 3', 90]] 
+                  };
+
     const saying = 'cool song 2';
 
     //your code goes here
     
-    expect(human.songs).to.deep.equal([['cool song 1', 50], ['cool song 3', 90]]);
+    expect(human.songs).to.deep.equal([['cool song 1', 50], 
+                                       ['cool song 3', 90]]);
   });
 
-  it.skip('should filter based off of strings from user ', () => {
+  it.skip('should filter message based off of strings from user ', () => {
     const userString = 'su';
     const messages = [{ user:'louisa', message: 'haaaaa' },
                       { user:'yung-jhun', message: 'kewl dude' },
                       { user: 'alter-nate', message:'suh kewl' },
                       { user:'meeka', message: 'adfasuhlkj' },
-                      { user: 'robbie', message: 'im super loud' }];
+                      { user: 'robbie', message: 'im loud' }];
 
     // your code goes here
 
 
     expect(results).to.deep.equal([{ user: 'alter-nate', message:'suh kewl' },
                                    { user:'meeka', message: 'adfasuhlkj' },
-                                   { user: 'robbie', message: 'im super loud' }]);
+                                   { user: 'robbie', message: 'im loud' }]);
   });
 
-  it.skip('should return the correct ids', () => {
+  it.skip('should return a new array with the correct ids', () => {
     const data = [{ account: 'person 1', id: '00112' },
                   { account: 'person 2', id: '00045' },
                   { account: 'person 3', id: '12309' },
